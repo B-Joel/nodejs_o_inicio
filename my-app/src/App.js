f598c5911bc1c169c737a3240105f71a100083ec
B-Joel <joelbellatto@hotmail.com>
2021-07-27 23:15:05 -0300

    tutorial internet
    Seguindo o passo a passo do tutorial
    
    https://www.luiztools.com.br/post/tutorial-de-reactjs-com-node-js-4/

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Button variant="primary">Learn React</Button>{' '}
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Routes />
+  )
 }
 
 export default App;
---
+++ my-app/src/Pages/Home.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import Header from "../Header";
+import logo from "../logo.svg";
+import {Link} from 'react-router-dom';
+
+function Home(){
+  const [contador,setContador] = useState(0);
+  function increment() {
+    setContador(contador+1)
+  }
+
+  return(
+    <div className="Home">
+      <Header title='Header Param' />
+      <img src={logo} className='App-logo' alt = 'logo' />
+      <p>
+        <input type='button' value = 'clique' onClick={increment} />
+      </p>
+      <p>{contador} Cliques!</p>
+      
+      <p>
+        <Link to="/cadastro">Acessar Cadsatro</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
